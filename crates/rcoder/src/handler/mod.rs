2d7b776571de516f565f82ecd0c2b45beb85fedc
guochao.song <soddygo@qq.com>
2025-10-24 19:10:21 +0800

    fix

--- crates/rcoder/src/handler/mod.rs
+++ crates/rcoder/src/handler/mod.rs
@@ -1,6 +1,7 @@
 //! HTTP 路由和处理器模块
 mod agent_cancel_handler;
 mod agent_session_notification;
+mod agent_status_handler;
 mod agent_stop_handler;
 mod chat_handler;
 mod health_handler;
@@ -9,6 +10,7 @@ pub mod proxy_handler_api;
 
 pub use agent_cancel_handler::*;
 pub use agent_session_notification::*;
+pub use agent_status_handler::*;
 pub use agent_stop_handler::*;
 pub use chat_handler::*;
 pub use health_handler::*;
